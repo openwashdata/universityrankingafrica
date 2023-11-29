16968735e8e536bda610fd405f5bf3096d4e5377
Skorik  Sophia <sskorik@ethz.ch>
2023-11-29 16:27:37 +0100

    added countries dataset

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -8,14 +8,15 @@ library(readr)
 library(readxl)
 library(dplyr)
 library(stringr)
+library(janitor)
 
 #read data
 ura_raw <- read_excel("./data-raw/African_Universities.xlsx")
+countries_raw <- read_excel("./data-raw/African_Countries.xlsx")
 
 
 # tidy data ---------------------------------------------------------------
-
-universityrankingafrica <- ura_raw %>%
+ranking <- ura_raw |>
   dplyr::mutate(
     across(
       c("Rank Africa", "Rank World","Impact Rank","Openness Rank","Excellence Rank","Number of Engineering Courses", "Years of study", "Yearly fee", "Number of Students"),
@@ -26,11 +27,23 @@ universityrankingafrica <- ura_raw %>%
       as.factor
     ),
     `access date` = as.Date(`access date`, format = "%d.%m.%Y")
-  )
+  ) |>
+  janitor::clean_names() |>
+  rename(colonial_power = colonial_power_at_independence,
+         academic_system = academic_system_used,
+         engineering_courses = number_of_engineering_courses)
 
+african_countries <- countries_raw |>
+  janitor::clean_names()
 
 ## write data
-usethis::use_data(universityrankingafrica, overwrite = TRUE)
+usethis::use_data(ranking, overwrite = TRUE)
 fs::dir_create(here::here("inst", "extdata")) #exports dataset in R
-write_csv(universityrankingafrica, here::here("inst/extdata/universityrankingafrica")) #exports dataset in csv and excel
-openxlsx::write.xlsx(universityrankingafrica,"inst/extdata/universityrankingafrica")
+write_csv(ranking, here::here("inst/extdata/ranking")) #exports dataset in csv and excel
+openxlsx::write.xlsx(ranking,"inst/extdata/ranking")
+
+usethis::use_data(african_countries, overwrite = TRUE)
+write_csv(african_countries, here::here("inst/extdata/african_countries")) #exports dataset in csv and excel
+openxlsx::write.xlsx(african_countries,"inst/extdata/african_countries")
+
+
